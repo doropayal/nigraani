599fb19f43a0f404787ffa2d5aed103647baf7bf
Payal Saini <128584215+doropayal@users.noreply.github.com>
2023-08-29 14:16:43 +0530

    Add files via upload

---
+++ ActivitySelection.java
@@ -0,0 +1,11 @@
+public class ActivitySelection 
+{
+    public static void printMaxActivities(int s[] , int f[] ,int n)
+    {
+        int i,j;
+        System.out.println("following activities are selcted");
+        i=0;
+        System.out.println(i + " ");
+        for()
+    }
+}
